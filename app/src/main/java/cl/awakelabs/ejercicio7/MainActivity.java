c699e1dd1b3dbd3ea500d212df647b7dbe7da8d4
Alex Muñoz <lomejor77@gmail.com>
2023-07-01 17:59:08 -0400

    Ejercicio 7 iniciado

---
+++ app/src/main/java/cl/awakelabs/ejercicio7/MainActivity.java
@@ -0,0 +1,18 @@
+package cl.awakelabs.ejercicio7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Bienvenido al Ejercicio 7", Toast.LENGTH_LONG).show();
+    }
+
+}
